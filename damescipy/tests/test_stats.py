0eea17c775dcd57c1d1da3d14ce35d0de0335463
David Arroyo Menéndez <davidam@gmail.com>
2021-03-07 23:42:46 +0100

    tests/test_stats.py: add chi and chi2

--- damescipy/tests/test_stats.py
+++ damescipy/tests/test_stats.py
@@ -208,3 +208,55 @@ class TestBasics(TestCase):
         ax.hist(r, density=True, histtype='stepfilled', alpha=0.2)
         ax.legend(loc='best', frameon=False)
         self.assertEqual(str(ax), "AxesSubplot(0.125,0.11;0.775x0.77)")
+
+
+    def test_chi(self):
+        from scipy.stats import chi
+        import matplotlib.pyplot as plt
+        fig, ax = plt.subplots(1, 1)
+
+        df = 78
+        mean, var, skew, kurt = chi.stats(df, moments='mvsk')
+
+        x = np.linspace(chi.ppf(0.01, df),
+                        chi.ppf(0.99, df), 100)
+        ax.plot(x, chi.pdf(x, df),
+                'r-', lw=5, alpha=0.6, label='chi pdf')
+
+        rv = chi(df)
+        ax.plot(x, rv.pdf(x), 'k-', lw=2, label='frozen pdf')
+
+        vals = chi.ppf([0.001, 0.5, 0.999], df)
+        np.allclose([0.001, 0.5, 0.999], chi.cdf(vals, df))
+
+        r = chi.rvs(df, size=1000)
+
+        ax.hist(r, density=True, histtype='stepfilled', alpha=0.2)
+        ax.legend(loc='best', frameon=False)
+        self.assertEqual(str(ax), "AxesSubplot(0.125,0.11;0.775x0.77)")
+
+
+    def test_chi2(self):
+        from scipy.stats import chi2
+        import matplotlib.pyplot as plt
+        fig, ax = plt.subplots(1, 1)
+
+        df = 55
+        mean, var, skew, kurt = chi2.stats(df, moments='mvsk')
+
+        x = np.linspace(chi2.ppf(0.01, df),
+                        chi2.ppf(0.99, df), 100)
+        ax.plot(x, chi2.pdf(x, df),
+                'r-', lw=5, alpha=0.6, label='chi2 pdf')
+
+        rv = chi2(df)
+        ax.plot(x, rv.pdf(x), 'k-', lw=2, label='frozen pdf')
+
+        vals = chi2.ppf([0.001, 0.5, 0.999], df)
+        np.allclose([0.001, 0.5, 0.999], chi2.cdf(vals, df))
+
+        r = chi2.rvs(df, size=1000)
+
+        ax.hist(r, density=True, histtype='stepfilled', alpha=0.2)
+        ax.legend(loc='best', frameon=False)
+        self.assertEqual(str(ax), "AxesSubplot(0.125,0.11;0.775x0.77)")
