8b9d38eb7a8c9a5596d42eac22659280d8fbf677
David Arroyo Menéndez <davidam@gmail.com>
2021-03-07 23:50:16 +0100

    tests/test_stats.py: add expon

--- damescipy/tests/test_stats.py
+++ damescipy/tests/test_stats.py
@@ -285,3 +285,22 @@ class TestBasics(TestCase):
         ax.hist(r, density=True, histtype='stepfilled', alpha=0.2)
         ax.legend(loc='best', frameon=False)
         self.assertEqual(str(ax), "AxesSubplot(0.125,0.11;0.775x0.77)")
+
+    def test_expon(self):
+        from scipy.stats import expon
+        import matplotlib.pyplot as plt
+        fig, ax = plt.subplots(1, 1)
+
+        mean, var, skew, kurt = expon.stats(moments='mvsk')
+
+        x = np.linspace(expon.ppf(0.01),
+                        expon.ppf(0.99), 100)
+        ax.plot(x, expon.pdf(x),
+                'r-', lw=5, alpha=0.6, label='expon pdf')
+
+        rv = expon()
+        ax.plot(x, rv.pdf(x), 'k-', lw=2, label='frozen pdf')
+
+        vals = expon.ppf([0.001, 0.5, 0.999])
+        np.allclose([0.001, 0.5, 0.999], expon.cdf(vals))
+        self.assertEqual(str(ax), "AxesSubplot(0.125,0.11;0.775x0.77)")
